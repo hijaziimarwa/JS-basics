d18e85d2b6cdfdb15cd0cbc70e336b6f2e6658cb
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-14 00:52:47 +0300

    Create students object

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const students = [
+  { name: "Alice", scores: [85, 90, 78] },  
+    { name: "Bob", scores: [88, 92, 95] },
+    { name: "Charlie", scores: [70, 75, 80] },
+    { name: "David", scores: [90, 85, 88] },
+]
