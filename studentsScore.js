c2eed2401dbaab70179f9f1de507a3807d0055c5
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-15 01:45:52 +0300

    Calculate average using the normal way

--- studentsScore.js
+++ studentsScore.js
@@ -1,6 +1,23 @@
 const students = [
-  {name:"Ahmad", score:[80, 90, 85]},
-  {name:"Ali", score:[70, 75, 80]},
+  {name:"Ahmad", score:[40, 60, 75]},
+  {name:"Ali", score:[70, 65, 80]},
   {name:"Sara", score:[90, 95, 100]}
 ];
-console.log(students);
+console.log(students); 
+
+function calculateAverage(){
+  
+  for(let i=0;i<students.length;i++){
+      let sum=0;
+      for(let j=0;j<students[i].score.length;j++){
+        sum+=students[i].score[j];
+      }
+      let avg=sum/students[i].score.length;
+    console.log(`Average score of ${students[i].name} is ${avg}`);
+  };
+  }
+function generateReports(){
+
+}
+calculateAverage();
+
