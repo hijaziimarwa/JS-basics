93cc169cadb04ba273e2fa22ebecadf07ad14286
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-15 03:01:26 +0300

    Print the final array of student objects

--- studentsScore.js
+++ studentsScore.js
@@ -3,14 +3,14 @@ const students = [
   {name:"Ali", score:[70, 65, 80]},
   {name:"Sara", score:[90, 95, 100]}
 ];
-console.log(students); 
+
 
 function calculateAverage(){
   let result=[];
   for(let i=0;i<students.length;i++){
     let sum = students[i].score.reduce((acc, score) => acc + score, 0);
     let avg = sum / students[i].score.length;
-    console.log(`Average score of ${students[i].name} is ${avg}`);
+    // console.log(`Average score of ${students[i].name} is ${avg}`);
     let studentObjects = {
       name: students[i].name,
       average: avg
@@ -33,9 +33,7 @@ function assignGrade(){
       result[i].grade = "C";
     }
 }}
-function generateReports(){
 
-}
 let result=calculateAverage();
-console.log(result);
 assignGrade();
+console.log(result);
