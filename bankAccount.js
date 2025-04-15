b33d4f6f5767c9042fc5e8f87c929ce390669b08
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-15 03:14:59 +0300

    Add methods to the class

--- bankAccount.js
+++ bankAccount.js
@@ -3,5 +3,37 @@ class BankAccount{
         this.ownerName = ownerName;
         this.balance = initialBalance;
     }
-    
+
+    deposit(amount){
+        this.balance += amount;
+        console.log(`Deposited ${amount}. New balance is ${this.balance}`);
+    }
+
+    withdraw(amount){
+        if (amount > this.balance) {
+            console.log(`Insufficient funds. Cannot withdraw ${amount}`);
+        } else {
+            this.balance -= amount;
+            console.log(`Withdrew ${amount}. New balance is ${this.balance}`);
+        }
+    }
+
+    transfer(amount, targetAccount){
+        if (amount > this.balance) {
+            console.log(`Insufficient funds. Cannot transfer ${amount}`);
+        } else {
+            this.balance -= amount;
+            targetAccount.deposit(amount);
+            console.log(`Transferred ${amount} to ${targetAccount.ownerName}. New balance is ${this.balance}`);
+        }
+    }
+    getSummary(){
+        console.log(`Account owner: ${this.ownerName}`);
+        console.log(`Current balance: ${this.balance}`);
+    }
+
+
 }
+
+const account1 = new BankAccount('Alice', 1000);
+const account2 = new BankAccount('Bob', 500);
