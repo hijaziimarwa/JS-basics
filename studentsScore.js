4e728edcc9f1ac2c175fd29d17a6b97dcb2bdaf3
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-15 03:04:23 +0300

    Format the file

--- studentsScore.js
+++ studentsScore.js
@@ -1,39 +1,36 @@
 const students = [
-  {name:"Ahmad", score:[40, 60, 75]},
-  {name:"Ali", score:[70, 65, 80]},
-  {name:"Sara", score:[90, 95, 100]}
+  { name: 'Ahmad', score: [40, 60, 75] },
+  { name: 'Ali', score: [70, 65, 80] },
+  { name: 'Sara', score: [90, 95, 100] },
 ];
 
-
-function calculateAverage(){
-  let result=[];
-  for(let i=0;i<students.length;i++){
+function calculateAverage() {
+  let result = [];
+  for (let i = 0; i < students.length; i++) {
     let sum = students[i].score.reduce((acc, score) => acc + score, 0);
     let avg = sum / students[i].score.length;
     // console.log(`Average score of ${students[i].name} is ${avg}`);
     let studentObjects = {
       name: students[i].name,
-      average: avg
+      average: avg,
     };
     result.push(studentObjects);
   }
   return result;
 }
 
-function assignGrade(){
- for(let i=0;i<result.length;i++){
-    if(result[i].average >= 80){
-      result[i].grade = "A";
+function assignGrade() {
+  for (let i = 0; i < result.length; i++) {
+    if (result[i].average >= 80) {
+      result[i].grade = 'A';
+    } else if (result[i].average >= 60 && result[i].average < 80) {
+      result[i].grade = 'B';
+    } else if (result[i].average < 60) {
+      result[i].grade = 'C';
     }
-    else if(result[i].average >= 60 && result[i].average < 80){
-      result[i].grade = "B";
-    }
-    
-    else if(result[i].average < 60){
-      result[i].grade = "C";
-    }
-}}
+  }
+}
 
-let result=calculateAverage();
+let result = calculateAverage();
 assignGrade();
 console.log(result);
