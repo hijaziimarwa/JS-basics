4dff0cafb1381d14a26bdac6daab5401144f2776
hijaziimarwa <hijaziimarwa@gmail.com>
2025-04-16 13:01:44 +0300

    Linking variable in js to the ones in html

--- toDoList.js
+++ toDoList.js
@@ -1,9 +1,38 @@
-let tasks=[]
+//i added an Add task button in htmk and want to functionality to add a task to the list
 
-function addTask(tasktext) {
-    tasks.push({task:tasktext, completed:false})
-    console.log(`Task added "${tasktext}".`)
-    }
+const taskInput = document.querySelector('.taskInput')
+const addTaskButton = document.querySelector('.addTaskBtn')
+const taskList = document.querySelector('.taskList')
 
-addTask('Complete homework')
-addTask('Buy groceries')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let tasks=[]
+
+// function addTask(tasktext) {
+//     tasks.push({task:tasktext, completed:false})
+//     console.log(`Task added "${tasktext}".`)
+//     }
+
+// addTask('Complete homework')
+// addTask('Buy groceries')
